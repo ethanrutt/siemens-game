8771729e35cd92d3d91113cc886862e8c04a9f01
Rishi Santhanam <rishisant@tamu.edu>
2024-11-21 20:17:48 -0600

    [FIX] Debug

--- Assets/Scripts/Character/PlayerData.cs
+++ Assets/Scripts/Character/PlayerData.cs
@@ -60,11 +60,11 @@ public class PlayerData : MonoBehaviour
     [SerializeField] public Dictionary<string, int> npc_interactions = new Dictionary<string, int>()
     {
         {"deckmaster", 0},
-        {"casino_owner", 1},
+        {"casino_owner", 0},
         {"shopkeeper", 0},
         {"drunkard", 0},
-        {"sensei", 2}, // just for debug
-        {"casinoenter", 1},
+        {"sensei", 0}, // just for debug
+        {"casinoenter", 0},
         {"labenter", 0}
     };
 
